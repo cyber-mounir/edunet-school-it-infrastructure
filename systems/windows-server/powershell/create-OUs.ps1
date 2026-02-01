037daab5c17c912c2bd15ef321765118abaa3b73
Mounir Tech <touflihtmounir@gmail.com>
2026-02-01 16:52:35 +0100

    Scripts PowerShell

---
+++ systems/windows-server/powershell/create-OUs.ps1
@@ -0,0 +1,22 @@
+
+Import-Module ActiveDirectory
+
+$Domain = "DC=edunet,DC=local"
+
+$OUs = @(
+    @{Name="Admin"; Description="OU pour l'administration et la direction"},
+    @{Name="Teachers"; Description="OU pour les enseignants"},
+    @{Name="Students"; Description="OU pour les élèves"},
+)
+
+
+foreach ($ou in $OUs) {
+    # تحقق إذا كان OU موجود بالفعل
+    $exists = Get-ADOrganizationalUnit -Filter "Name -eq '$($ou.Name)'" -SearchBase $Domain -ErrorAction SilentlyContinue
+    if (-not $exists) {
+        New-ADOrganizationalUnit -Name $ou.Name -Path $Domain -Description $ou.Description -ProtectedFromAccidentalDeletion $true
+        Write-Host "✅ OU '$($ou.Name)' created successfully."
+    } else {
+        Write-Host "⚠ OU '$($ou.Name)' already exists."
+    }
+}
