8fff00789a940f13c8b3f274d5ea6c4d99abea59
Mounir Tech <touflihtmounir@gmail.com>
2026-02-01 12:33:28 +0100

    Update bulk-students.ps1

--- systems/windows-server/powershell/bulk-students.ps1
+++ systems/windows-server/powershell/bulk-students.ps1
@@ -10,7 +10,7 @@ Get-Content $StudentFile | ForEach-Object {
     $Parts = $FullName.Split(" ")   # "firstname lastname"
     $fn = $Parts[0]                 # first name
     $ln = $Parts[1]                 # last name
-    $user = ($i + "." + $fn.Substring(0,1) + $ln).ToLower()
+    $user = ($i + "." + $ln.Substring(0,2) + $fn).ToLower()
 
     New-ADUser -Name $FullName `
         -SamAccountName $user `
