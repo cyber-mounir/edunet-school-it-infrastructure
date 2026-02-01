7c0e2135d6380475fb49c9c1b0e2ded2af053559
Mounir Tech <touflihtmounir@gmail.com>
2026-02-01 12:34:33 +0100

    Update bulk-students.ps1

--- systems/windows-server/powershell/bulk-students.ps1
+++ systems/windows-server/powershell/bulk-students.ps1
@@ -12,7 +12,7 @@ Get-Content $StudentFile | ForEach-Object {
     $ln = $Parts[1]                 # last name
     $user = ($i + "." + $ln.Substring(0,2) + $fn).ToLower()
 
-    New-ADUser 
+    New-ADUser ` 
         -Name $FullName `
         -SamAccountName $user `
         -UserPrincipalName "$user@edunet.local" `
